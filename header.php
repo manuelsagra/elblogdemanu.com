826319c2e59728f78b02e178cd14edf74e557150
Manuel Sagra de Diego <manuelsagra@gmail.com>
2017-05-23 12:39:56 +0200

    Header dates

--- header.php
+++ header.php
@@ -48,7 +48,7 @@ if (is_single()) {
 }
 ?>">
 
-		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?20170523">
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?20170523b">
 
 		<link rel="alternate" type="application/rss+xml" title="RSS" href="http://feeds.feedburner.com/ElBlogDeManu">
 		<?php if (is_single()) : ?><link rel="canonical" href="<?php the_permalink(); ?>"><?php endif; ?>
