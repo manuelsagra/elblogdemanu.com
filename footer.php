c8ebeb98409db257dedf6c549b7a7c794031e116
Manuel Sagra de Diego <manuelsagra@gmail.com>
2018-07-12 18:55:40 +0200

    Some changes to make the theme faster to load

--- footer.php
+++ footer.php
@@ -4,21 +4,27 @@
 			<div class="banners wrapper">
 				<p><i class="fa fa-external-link"></i> También te puede interesar</p>
 				<div class="clearfix">
-					<a href="http://paraisofriki.com/" class="banner pf"><img src="<?php bloginfo('template_directory'); ?>/img/pf.png" alt="Paraíso Friki" title="Paraíso Friki"></a>
-					<a href="http://retroescena.com/" class="banner re"><img src="<?php bloginfo('template_directory'); ?>/img/re.png" alt="RetroEscena" title="RetroEscena"></a>
+				<a href="http://paraisofriki.com/" class="banner pf"><img src="<?php bloginfo('template_directory'); ?>/img/pf.png" alt="Paraíso Friki" title="Paraíso Friki"></a>
+				<a href="http://retroescena.com/" class="banner re"><img src="<?php bloginfo('template_directory'); ?>/img/re.png" alt="RetroEscena" title="RetroEscena"></a>
 				</div>
 				<p class="affiliated"><i class="fa fa-trophy"></i> Partner</p>
 				<div class="clearfix">
-					<div class="affiliate"><a href="http://www.paisdelosjuegos.com.mx/" target="_blank">Pais de los Juegos</a>
-						<span>Juega a miles de juegos gratuitos</span>
-					</div>
-					<div class="affiliate"><a href="http://www.juegosinfantiles.com" target="_blank">Juegosinfantiles.com</a>
-					</div>
 					<div class="affiliate"><a href="http://juegos-casino.org/" target="_blank">juegos-casino.org</a></div>
 					<div class="affiliate"><a href="http://www.casinocarlos.com" target="_blank">CasinoCarlos.com</a></div>
 					<div class="affiliate"><a href="http://www.casinosenvivo.es/ruleta/" target="_blank">Ruleta en Vivo</a></div>
+					<div class="affiliate"><a href="http://www.thecasinocity.es/" target="_blank">thecasinocity.es</a></div>
+					<div class="affiliate"><a href="https://www.srcasino.es/" target="_blank">Srcasino.es</a></div>
+					<div class="affiliate"><a href="https://neonslots.es/" target="_blank">NeonSlots</a></div>
 <div style="clear:both"></div>
-					<?php if (is_front_page() && !is_paged()) { ?><div class="affiliate" style="margin-top:1em"><a href="http://www.casinosonlineespana.org/" target="_blank">www.casinosonlineespana.org</a></div><?php } ?>
+					<?php if (is_front_page() && !is_paged()) { ?>
+						<div style="margin-top:1em">
+							<div class="affiliate"><a href="http://www.casinosonlineespana.org/" target="_blank">www.casinosonlineespana.org</a></div>
+							<div class="affiliate"><a href="https://www.tragaperras-online.org/" target="_blank">www.tragaperras-online.org</a></div>
+							<div class="affiliate"><a href="http://www.ruleta-casino.es/" target="_blank">www.ruleta-casino.es</a></div><div style="clear:both"></div>
+						</div>						
+					<?php } ?>
+					<div class="affiliate" style="margin-top:1em"><a href="http://www.juegosdecasino.net/" target="_blank">Juegos de Casino Gratis</a> – Juega en el portal de casino más completo ¡Le aseguramos diversión y una experiencia tan intensa como real!</div>
+					<div class="affiliate" style="margin-top:1em">Encuentra los mejores bonos, reseñas reales e increíbles crupiers en vivo. ¡Únete a los líderes del casino en vivo en <a href="http://livecasino.com/es/" target="_blank">LiveCasino.com</a>!</div>
 				</div>
 			</div>
 			<footer>
